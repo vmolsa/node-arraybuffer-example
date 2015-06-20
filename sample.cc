805ea3daf10d40f98ed13efff7e13ef55845c8df
vmolsa <ville.molsa@gmail.com>
2015-06-21 01:28:37 +0300

    Update sample

--- sample.cc
+++ sample.cc
@@ -4,6 +4,8 @@
 
 using namespace v8;
 
+#if NODE_MINOR_VERSION > 11
+
 void CreateArrayBuffer(const v8::FunctionCallbackInfo<v8::Value>& args) {
   Isolate *isolate = args.GetIsolate();
   
@@ -22,8 +24,7 @@ void PrintArrayBuffer(const v8::FunctionCallbackInfo<v8::Value>& args) {
 
 void DisposeMemory(const v8::FunctionCallbackInfo<v8::Value>& args) {
   Isolate *isolate = args.GetIsolate();
- 
-  while(!isolate->IdleNotification(1000));
+  isolate->LowMemoryNotification();
 }
 
 void SampleInit(Handle<Object> exports) {
@@ -39,5 +40,44 @@ void SampleInit(Handle<Object> exports) {
   exports->Set(String::NewFromUtf8(isolate, "DisposeMemory"), 
                FunctionTemplate::New(isolate, DisposeMemory)->GetFunction());
 }
+
+#else
+
+static Handle<Value> CreateArrayBuffer(const Arguments& args) {
+  HandleScope scope;
+  node::ArrayBuffer *buffer = node::ArrayBuffer::New("Hello World!");
+  return scope.Close(buffer->ToArrayBuffer());
+}
+
+static Handle<Value> PrintArrayBuffer(const Arguments& args) {
+  HandleScope scope;
+  
+  node::ArrayBuffer *buffer = node::ArrayBuffer::New(args[0]);
+  
+  printf("node::ArrayBuffer: '%s', %zu\n", buffer->ToUtf8(), buffer->Length());
+  
+  return scope.Close(Undefined());
+}
+
+static Handle<Value> DisposeMemory(const Arguments& args) {
+  HandleScope scope;
+  V8::LowMemoryNotification();
+  return scope.Close(Undefined());
+}
+
+void SampleInit(Handle<Object> exports) {
+  HandleScope scope;
+  
+  exports->Set(String::New("CreateArrayBuffer"), 
+               FunctionTemplate::New(CreateArrayBuffer)->GetFunction());
+  
+  exports->Set(String::New("PrintArrayBuffer"), 
+               FunctionTemplate::New(PrintArrayBuffer)->GetFunction());
+               
+  exports->Set(String::New("DisposeMemory"), 
+               FunctionTemplate::New(DisposeMemory)->GetFunction());
+}
+
+#endif
 
 NODE_MODULE(sample, SampleInit);
--- sample.js
+++ sample.js
@@ -3,8 +3,12 @@ var sample = require('./build/Release/sample.node');
 function PrintJS(arrayBuffer) {
   var buffer = new Buffer(new Uint8Array(arrayBuffer));
   
-  if (Buffer.byteLength(buffer, 'utf8')) {  
-    console.log('JS:', "'" + buffer.toString('utf8') + "'", Buffer.byteLength(buffer, 'utf8'));
+  try {
+    if (Buffer.byteLength(buffer, 'utf8')) {  
+      console.log('JS:', "'" + buffer.toString('utf8') + "'", Buffer.byteLength(buffer, 'utf8'));
+    }
+  } catch(ignored) {
+    console.log('JS:', buffer.toString('utf8'));
   }
 }
 
@@ -18,8 +22,9 @@ buf.write('HELLO', 0);
 
 var arrayBuffer = new Uint8Array(buf).buffer;
 
-Print(arrayBuffer);
-Print('New ArrayBuffer From String!');
-Print(sample.CreateArrayBuffer());
-
-sample.DisposeMemory();
+for (var index = 0; index < 100; index++) {
+  Print(arrayBuffer);
+  Print('New ArrayBuffer From String!');
+  Print(sample.CreateArrayBuffer());
+  sample.DisposeMemory();
+}
